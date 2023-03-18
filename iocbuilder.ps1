502ae490c8c36fc66ee7e6e4763ae5824a20d8d6
Nathan <127637597+nwjohns101@users.noreply.github.com>
2023-03-18 21:46:11 +1100

    Major code revision
    Changed the program so that it accepts a CSV using a file dialog, gives the user the options of defanging or refanging and then lets the user select the save location using file dialog. Removed functionality for processing .XLSX, program only handles .CSV for simplicity

--- iocbuilder.ps1
+++ iocbuilder.ps1
@@ -1,6 +1,5 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
-Add-Type -Path ".\Microsoft.Office.Interop.Excel.dll"
 
 # Set dimensions for PowerShell window
 
@@ -20,93 +19,58 @@ Write-Host -ForegroundColor White '  888  Y88b. .d88P Y88b  d88P      888   d88P
 Write-Host -ForegroundColor White '8888888 "Y88888P"   "Y8888P"       8888888P"   "Y88888 888 888  "Y88888  "Y8888  888     '
 #Banner end
 
-Write-Host ""
-Write-Host "Please select your file for processing.."
-
-# Prompt user to select file
+# Show an open file dialog to let the user select a CSV file
 $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-$openFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx|CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
-$openFileDialog.Title = "Select a file"
-$dialogResult = $openFileDialog.ShowDialog()
+$openFileDialog.Filter = "CSV Files (*.csv)|*.csv"
+$openFileDialog.Title = "Select CSV file"
+$openFileDialog.ShowDialog() | Out-Null
 
-if ($dialogResult -ne [System.Windows.Forms.DialogResult]::OK) {
-    Write-Host "No file selected"
-    return
+if ([string]::IsNullOrEmpty($openFileDialog.FileName)) {
+    Write-Host "No file selected. Exiting script."
+    Exit
 }
 
-$excelFilePath = $openFileDialog.FileName
+# Ask the user if they want to refang or defang
+$choice = Read-Host "Do you want to refang or defang? Enter 'r' for refang and 'd' for defang"
 
-# Load Excel file
-$excel = New-Object -ComObject Excel.Application
-$workbook = $excel.Workbooks.Open($excelFilePath)
-$worksheet = $workbook.ActiveSheet
+# Check if the user entered a valid choice
+if ($choice -ne 'r' -and $choice -ne 'd') {
+    Write-Host "Invalid choice. Exiting script."
+    Exit
+}
 
-# Find the next available column and insert the URL and IP header
-$lastColumn = $worksheet.UsedRange.Columns.Count
-$urlColumnIndex = $lastColumn + 1
-$urlColumn = $worksheet.Cells.Item(1, $urlColumnIndex)
-$urlColumn.Value2 = "URLs"
-$ipColumnIndex = $lastColumn + 2
-$ipColumn = $worksheet.Cells.Item(1, $ipColumnIndex)
-$ipColumn.Value2 = "IP Addresses"
+# Load the CSV file
+$data = Import-Csv $openFileDialog.FileName
 
-# Get any values that look like a URL or IP address and put them in the new columns
-$range = $worksheet.UsedRange
-$urlRegex = "(?:https?://|www\.)[\w\-]+(?:\.[\w\-]+)+[/#?]?.*?(?=[\s]|$|\(|\))"
-$ipRegex = "\b(?:\d{1,3}\.){3}\d{1,3}\b"
-foreach ($cell in $range.Cells) {
-    $value = $cell.Value2
-    if ($value -match $urlRegex) {
-        # Remove parentheses from URL
-        $url = $Matches[0] -replace '[()]'
-        $urlCell = $worksheet.Cells.Item($cell.Row, $urlColumnIndex)
-        $urlCell.Value2 = $url
-    }
-    if ($value -match $ipRegex) {
-        $ipCell = $worksheet.Cells.Item($cell.Row, $ipColumnIndex)
-        $ipCell.Value2 = $value
+# Iterate over each row in the CSV and refang/defang the specified columns
+foreach ($row in $data) {
+    foreach ($column in $row.PSObject.Properties) {
+        if ($column.Value -is [string]) {
+            if ($choice -eq 'r') {
+                # Refang any IP address or URL found in the column
+                $column.Value = $column.Value -replace '\[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\]', '$1'
+                $column.Value = $column.Value -replace '\[(\w+://[^/]+)(/.+)?\]', '$1$2'
+                $column.Value = $column.Value -replace '\[(\.|:)]', '$1'
+            } else {
+                # Defang any IP address or URL found in the column
+                $column.Value = $column.Value -replace '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', '[$1]'
+                $column.Value = $column.Value -replace '(\.|:)', '[$&]'
+            }
+        }
     }
 }
 
-# Remove any empty cells under the URL and IP columns
-$lastRow = $worksheet.UsedRange.Rows.Count
-$urlRange = $worksheet.Range($worksheet.Cells.Item(2, $urlColumnIndex), $worksheet.Cells.Item($lastRow, $urlColumnIndex))
-$urlRange.SpecialCells([Microsoft.Office.Interop.Excel.XlCellType]::xlCellTypeBlanks).Delete()
-$ipRange = $worksheet.Range($worksheet.Cells.Item(2, $ipColumnIndex), $worksheet.Cells.Item($lastRow, $ipColumnIndex))
-$ipRange.SpecialCells([Microsoft.Office.Interop.Excel.XlCellType]::xlCellTypeBlanks).Delete()
-
-Write-Host "Processing complete.."
-
-# Display dialog box to prompt user for file name and path to save
+# Show a save file dialog to let the user save the modified CSV as a new file
 $saveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
-$saveFileDialog.Title = "Save File"
-$saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx|CSV Files (*.csv)|*.csv"
+$saveFileDialog.Filter = "CSV Files (*.csv)|*.csv"
+$saveFileDialog.Title = "Save modified CSV file"
 $saveFileDialog.ShowDialog() | Out-Null
 
-# Get the file name and path from the dialog box
-$filePath = $saveFileDialog.FileName
-$fileExtension = [System.IO.Path]::GetExtension($filePath)
-
-# Save as either Excel or CSV
-if ($fileExtension -eq ".xlsx") {
-    $worksheet.SaveAs($filePath, [Microsoft.Office.Interop.Excel.XlFileFormat]::xlOpenXMLWorkbook)
-}
-elseif ($fileExtension -eq ".csv") {
-    $worksheet.SaveAs($filePath, [Microsoft.Office.Interop.Excel.XlFileFormat]::xlCSV)
-}
-else {
-    Write-Host "Invalid file type selected"
-    return
+if ([string]::IsNullOrEmpty($saveFileDialog.FileName)) {
+    Write-Host "No file selected. Exiting script."
+    Exit
 }
 
-# Clean up
-$workbook.Close($false)
-$excel.Quit()
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet) | Out-Null
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($workbook) | Out-Null
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
+$data | Export-Csv $saveFileDialog.FileName -NoTypeInformation
 
-Write-Host "File saved as $csvFilePath"
-
-# Kill excel processes that are not visible
-Get-Process Excel | Where-Object {$_.MainWindowTitle -eq ''} | Stop-Process
+Write-Host "CSV file has been modified and saved as $($saveFileDialog.FileName)"
